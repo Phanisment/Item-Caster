f3c5ae42e6cb1679aa4a86fe8f2959d23ed06a22
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-29 22:09:18 +0700

    update src/io/phanisment/itemcaster/api/CastSkill.java

--- src/io/phanisment/itemcaster/api/CastSkill.java
+++ src/io/phanisment/itemcaster/api/CastSkill.java
@@ -11,16 +11,15 @@ import org.bukkit.entity.Player;
 
 import java.util.HashSet;
 
-public class MythicMobsCompat {
-        public static void runSkill(String id, Player player) {
-                if (id == null || id.isEmpty()) return;
-
-                MythicBukkit.inst().getSkillManager().getSkill(id).ifPresent(skill -> {
-                        AbstractPlayer trigger = BukkitAdapter.adapt(player);
-                        GenericCaster genericCaster = new GenericCaster(trigger);
-                        SkillMetadata skillMeta = new SkillMetadataImpl(SkillTriggers.API, genericCaster, trigger, BukkitAdapter.adapt(player.getLocation()),
-                                        new HashSet<>(), null, 1.0F);
-                        if (skill.isUsable(skillMeta, SkillTriggers.API)) skill.execute(skillMeta);
-                });
-        }
+public class CastSkill {
+	public static void runSkill(String id, Player player) {
+	if (id == null || id.isEmpty()) return;
+		MythicBukkit.inst().getSkillManager().getSkill(id).ifPresent(skill -> {
+			AbstractPlayer trigger = BukkitAdapter.adapt(player);
+			GenericCaster genericCaster = new GenericCaster(trigger);
+			SkillMetadata skillMeta = new SkillMetadataImpl(SkillTriggers.API, genericCaster, trigger, BukkitAdapter.adapt(player.getLocation()),
+			new HashSet<>(), null, 1.0F);
+			if (skill.isUsable(skillMeta, SkillTriggers.API)) skill.execute(skillMeta);
+		});
+	}
 }
