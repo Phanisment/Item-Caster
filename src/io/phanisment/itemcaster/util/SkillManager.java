bfe9f0531af64aa180983552b3f9fff355d5f7a0
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-31 10:58:10 +0700

    update src/io/phanisment/itemcaster/util/SkillManager.java

---
+++ src/io/phanisment/itemcaster/util/SkillManager.java
@@ -0,0 +1,51 @@
+package io.phanisment.itemcaster.util;
+
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NBTListCompound;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AbilityManager {
+	public List<Abilites> getAbilites(ItemStack item) {
+		if(item == null || !item.hasItemMeta()){
+			return null;
+		}
+		
+		NBTItem itemNbt = new NBTItem(item);
+		List<Abilites> abilites = new ArrayList<>();
+		for(NBTListCompound itemNbt : itemNbt.getCompoundList("Abilites")) {
+			String id = itemNbt.getString("id");
+			String event = itemNbt.hasKey("event") ? itemNbt.getString("event") : null;
+			Integer timer = itemNbt.hasKey("timer") ? itemNbt.getInteger("timer") : null;
+			abilities.add(new Ability(id, event, timer));
+		}
+			return abilities;
+		}
+	}
+	
+	public static class Ability {
+		private final id;
+		private final event;
+		private final timer;
+		
+		public String getId() {
+			return id;
+		}
+		
+		public String getEvent() {
+			return event;
+		}
+		
+		public int getTimer() {
+			return timer;
+		}
+		
+		public String toString() {
+			return "Ability{id='" + id + "', event='" + event + "', timer=" + timer + "}";
+		}
+	}
+}
