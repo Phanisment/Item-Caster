4c644deb4644fdf6cf5b6061ac7cb606243c3a61
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-31 11:36:55 +0700

    update src/io/phanisment/itemcaster/ItemCaster.java

--- src/io/phanisment/itemcaster/ItemCaster.java
+++ src/io/phanisment/itemcaster/ItemCaster.java
@@ -2,9 +2,13 @@ package io.phanisment.itemcaster;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.phanisment.itemcaster.util.PassiveSkill;
+
 public class ItemCaster extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getLogger().info("Plugin Installed!")
+		
+		new PassiveSkill(this);
 	}
 }
