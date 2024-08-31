a7fca8452def3844f31294606273836e6959bd1c
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-31 12:34:03 +0700

    update src/main/java/io/phanisment/itemcaster/util/PassiveSkill.java

--- src/main/java/io/phanisment/itemcaster/util/PassiveSkill.java
+++ src/main/java/io/phanisment/itemcaster/util/PassiveSkill.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class PassiveSkill {
 	private final ItemCaster plugin;
-	private final AbilityManager nbtAbility = new AbilityManager()
+	private final AbilityManager nbtAbility = new AbilityManager();
 	
 	public PassiveSkill(ItemCaster plugin) {
 		this.plugin = plugin;
