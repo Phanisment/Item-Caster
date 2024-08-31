db52b2d89f1c7b1853350eca807868ceff40ac74
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-31 14:23:52 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -2,18 +2,11 @@ package io.phanisment.itemcaster;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import de.tr7zw.changeme.nbtapi.NBT;
-import io.phanisment.itemcaster.util.PassiveSkill;
+import io.phanisment.nbtapi.NBT;
 
 public class ItemCaster extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getLogger().info("Plugin Installed!");
-		if (!NBT.preloadApi()) {
-			getLogger().warning("NBT-API wasn't initialized properly, disabling the plugin");
-			getPluginLoader().disablePlugin(this);
-			return;
-		}
-		new PassiveSkill(this);
 	}
 }
