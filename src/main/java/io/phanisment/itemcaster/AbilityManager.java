51108c2af572eab341cc4a31e34618413f1dafce
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-31 22:10:21 +0700

    update src/main/java/io/phanisment/itemcaster/AbilityManager.java

--- src/main/java/io/phanisment/itemcaster/AbilityManager.java
+++ src/main/java/io/phanisment/itemcaster/AbilityManager.java
@@ -2,20 +2,20 @@ package io.phanisment.itemcaster;
 
 import org.bukkit.inventory.ItemStack;
 
+import de.tr7zw.changeme.nbtapi.NBTCompoundList;
 import de.tr7zw.changeme.nbtapi.NBTCompound;
 import de.tr7zw.changeme.nbtapi.NBTItem;
-import de.tr7zw.changeme.nbtapi.NBTList;
 
 public class AbilityManager {
 	public String getNbt(ItemStack item) {
 		NBTItem nbtItem = new NBTItem(item);
 		NBTCompound abilities = nbtItem.getCompound("Abilities");
 		if(abilities != null) {
-			NBTList<NBTCompound> listAbility = abilities.getCompoundList("Abilities");
+			NBTCompoundList listAbility = abilities.getCompoundList("Abilities");
 			for(NBTCompound ability : listAbility) {
 				String skill = ability.getString("skill");
 				String event = ability.getString("event");
-				int timer = ability.getInt("timer");
+				int timer = ability.getInteger("timer");
 			}
 		} else {
 			return null;
