ad5be98599fcea6aaf34c99bc4584d162475d24b
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-31 14:25:29 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -2,8 +2,6 @@ package io.phanisment.itemcaster;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import io.phanisment.nbtapi.NBT;
-
 public class ItemCaster extends JavaPlugin {
 	@Override
 	public void onEnable() {
