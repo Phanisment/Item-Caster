6563b055f9c9b30df84f12d5ac1823daaac42dd9
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-31 10:26:51 +0700

    update src/io/phanisment/itemcaster/util/PassiveSkill.java

--- src/io/phanisment/itemcaster/util/PassiveSkill.java
+++ src/io/phanisment/itemcaster/util/PassiveSkill.java
@@ -1,1 +1,24 @@
+package io.phanisment.itemcaster.util;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import io.phanisment.itemcaster.ItemCaster
+
+public class PassiveSkill {
+	private final ItemCaster plugin;
+	
+	public PassiveSkillChecker(ItemCaster plugin) {
+		this.plugin = plugin;
+		
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					/* Code */
+				}
+			}
+		}.runTaskTimer(plugin, 0L, 1L);
+	}
+}
