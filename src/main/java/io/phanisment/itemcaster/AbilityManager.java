fcc1079ca886d865754289861ebd964dfbbfd0e5
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-01 13:21:41 +0700

    update src/main/java/io/phanisment/itemcaster/AbilityManager.java

--- src/main/java/io/phanisment/itemcaster/AbilityManager.java
+++ src/main/java/io/phanisment/itemcaster/AbilityManager.java
@@ -1,12 +1,27 @@
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.item.ItemStack;
-import org.bukkit.craftbukkit.v1_20_R0.inventory.CraftItemStack;
+package io.phanisment.itemcaster;
+
 import org.bukkit.inventory.ItemStack;
 
-public class NBTUtil {
-	public static NBTTagCompound getNBTTag(BukkitItemStack item) {
-		ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-		NBTTagCompound nbtTag = (nmsItem.hasTag()) ? nmsItem.getTag() : new NBTTagCompound();
-		return nbtTag;
+import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
+import de.tr7zw.changeme.nbtapi.NBTCompoundList;
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+
+public class AbilityManager {
+	public static getSkill(ItemStack item) {
+		NBTItem nbtItem = new NBTItem(item);
+		NBTCompound abilities = nbtItem.getCompound("Abilities");
+		if(abilities != null) {
+			NBTCompoundList listAbility = abilities.getCompoundList("Abilities");
+			for(ReadWriteNBT ability : listAbility) {
+				String skill = ability.getString("skill");
+				String event = ability.getString("event");
+				int timer = ability.getInteger("timer");
+				String toString = "{Skill" + skill + ",Event:" + event + ",Timer:" + timer + "}";
+				return toString;
+			} else {
+				return null;
+			}
+		}
 	}
 }
