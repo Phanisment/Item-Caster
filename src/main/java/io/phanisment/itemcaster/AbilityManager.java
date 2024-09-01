de790b4312eadc65a468561c06686fb61cf19a20
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-01 13:24:53 +0700

    update src/main/java/io/phanisment/itemcaster/AbilityManager.java

--- src/main/java/io/phanisment/itemcaster/AbilityManager.java
+++ src/main/java/io/phanisment/itemcaster/AbilityManager.java
@@ -8,7 +8,7 @@ import de.tr7zw.changeme.nbtapi.NBTCompound;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 
 public class AbilityManager {
-	public static getSkill(ItemStack item) {
+	public static String getSkill(ItemStack item) {
 		NBTItem nbtItem = new NBTItem(item);
 		NBTCompound abilities = nbtItem.getCompound("Abilities");
 		if(abilities != null) {
