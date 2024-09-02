7172ef4225ec5fffdc95752891e2b99019317cb5
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-02 19:27:49 +0700

    update src/main/java/io/phanisment/itemcaster/api/Ability.java

--- src/main/java/io/phanisment/itemcaster/api/Ability.java
+++ src/main/java/io/phanisment/itemcaster/api/Ability.java
@@ -1,5 +1,15 @@
 package io.phanisment.itemcaster.api;
 
+import io.phanisment.itemcaster.AbilityManager;
+
 public class Ability {
-  
+	private final AbilityManager skill;
+	private final AbilityManager event;
+	private final AbilityManager timer;
+	
+	public Ability(String skill, String event, int timer) {
+		this.skill = skill;
+		this.event = event;
+		this.timer = timer;
+	}
 }
