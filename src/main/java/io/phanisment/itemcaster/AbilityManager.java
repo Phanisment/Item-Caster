e4a4f6fe143602162c2d24f2912bfda1f78a9f55
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-02 11:25:58 +0700

    Update AbilityManager.java

--- src/main/java/io/phanisment/itemcaster/AbilityManager.java
+++ src/main/java/io/phanisment/itemcaster/AbilityManager.java
@@ -8,8 +8,13 @@ import de.tr7zw.changeme.nbtapi.NBTCompoundList;
 import de.tr7zw.changeme.nbtapi.NBTCompound;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AbilityManager {
-	public Object AbilityManager(Player player) {
+	private List<Ability> abilities:
+	
+	public AbilityManager(Player player) {
 		ItemStack item = player.getInventory().getItemInHand();
 		NBTItem nbtItem = new NBTItem(item);
 		NBTCompound abilities = nbtItem.getCompound("Abilities");
@@ -19,9 +24,37 @@ public class AbilityManager {
 				String skill = ability.getString("skill");
 				String event = ability.getString("event");
 				int timer = ability.getInteger("timer");
+				this.abilities.add(new Ability(skill, event, timer));
 			}
 			return abilities;
 		}
 		return null;
+	}
+
+	public List<Ability> getAbility() {
+		return abilities
+	}
+
+	public static class Ability {
+		private String skill;
+		private String event;
+		private int timer;
+
+		public Ability(String skill, String event, int timer) {
+			this.skill = skill;
+			this.event = event;
+			this.timer = timer;
+		}
+
+		public String getSkill() {
+			return skill;
+		}
+
+		public String getEvent() {
+			return event;
+		}
+
+	public int getTimer() {
+		return timer;
 	}
 }
