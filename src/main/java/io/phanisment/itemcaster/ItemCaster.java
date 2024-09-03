1c6b54759df8091e0a6d520f1704fe69f81f977c
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-03 19:47:27 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -1,10 +1,12 @@
 package io.phanisment.itemcaster;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.block.Action;
+import org.bukkit.Material;
 import org .bukkit.entity.Player;
 
 import io.phanisment.itemcaster.AbilityManager;
@@ -19,16 +21,12 @@ public class ItemCaster extends JavaPlugin implements Listener {
 	
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
-		if(event.getAction() == Action.RIGHT_CLICK || event.getItem() != null) {
-			Player player = event.getPlayer();
-			AbilityManager nbt = AbilityManager(player);
+		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			AbilityManager nbt = AbilityManager(event.getItem());
 			String skill = nbt.getSkill();
 			String event = nbt.getEvent();
-			int timer = nbt.getTimer();
-			
-			player.sendMessage(skill);
-			player.sendMessage(event);
-			player.sendMessage(timer);
+			int timer = nbt.getTimer().toString();
+			Bukkit.broadcastMessage("Skill: " + skill + ", Event: " + event + ", Timer: " + timer);
 		}
 	}
 }
