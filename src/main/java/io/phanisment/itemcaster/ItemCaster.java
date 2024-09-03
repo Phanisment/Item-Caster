c7ba2b40c89f2eac370deccbdd3f0d0d2ec1ec80
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-03 19:57:46 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -1,14 +1,11 @@
 package io.phanisment.itemcaster;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.block.Action;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import io.phanisment.itemcaster.AbilityManager;
 
@@ -21,15 +18,22 @@ public class ItemCaster extends JavaPlugin implements Listener {
 	}
 	
 	@EventHandler
-	public void onInteract(PlayerInteractEvent event) {
-		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-			Player player = event.getPlayer();
-			ItemStack item = player..getInventory().getItemInHand();
-			AbilityManager nbt = AbilityManager(item);
-			String skill = nbt.getSkill();
-			String event = nbt.getEvent();
-			String timer = nbt.getTimer().toString();
-			Bukkit.broadcastMessage("Skill: " + skill + ", Event: " + event + ", Timer: " + timer);
+	public void onPlayerRightClick(PlayerInteractEvent event) {
+		switch (event.getAction()) {
+			case RIGHT_CLICK_AIR:
+			case RIGHT_CLICK_BLOCK:
+				Player player = event.getPlayer();
+				ItemStack item = player.getInventory().getItemInMainHand();
+				AbilityManager abilityManager = new AbilityManager(item);
+				String skill = abilityManager.getSkill();
+				if (skill != null) {
+					player.sendMessage("You have activated skill: " + skill);
+				} else {
+					player.sendMessage("This item has no abilities.");
+				}
+				break;
+			default:
+				break;
 		}
 	}
 }
