b4114b6c1d868aaf4d38bb77339c43df82cb993b
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-03 10:06:25 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -1,10 +1,31 @@
 package io.phanisment.itemcaster;
 
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.Action;
+import org .bukkit.entity.Player;
 
-public class ItemCaster extends JavaPlugin {
+import io.phanisment.itemcaster.AbilityManager;
+
+public class ItemCaster extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		getLogger().info("Plugin Installed!");
+		
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractEvent event) {
+		if(event.getAction() == Action.RIGHT_CLICK || event.getItem() != null) {
+			Player player = event.getPlayer();
+			AbilityManager nbt = AbilityManager(player);
+			
+			player.sendMessage(nbt.getSkill());
+			player.sendMessage(nbt.getEvent());
+			player.sendMessage(nbt.getTimer());
+		}
 	}
 }
