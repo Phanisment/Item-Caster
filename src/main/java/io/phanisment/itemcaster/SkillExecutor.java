39bbafa62c999909b73bb60e177ff100db4407c7
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-08 21:28:13 +0700

    update src/main/java/io/phanisment/itemcaster/SkillExecutor.java

--- src/main/java/io/phanisment/itemcaster/SkillExecutor.java
+++ src/main/java/io/phanisment/itemcaster/SkillExecutor.java
@@ -7,14 +7,26 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.event.block.Action;
 
-import io.phanisment.itemcaster.AbilityManager;
+import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
+import de.tr7zw.changeme.nbtapi.NBTCompoundList;
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTItem;
 
 public class SkillExecutor implements Listener {
 	@EventHandler
 	public void onPlayerRightClick(PlayerInteractEvent event) {
 		if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getItem() != null) {
-			Player player = event.getPlayer();
-			new AbilityManager(player);
+			ItemStack item = player.getInventory().getItemInMainHand();
+			NBTItem nbtItem = new NBTItem(item);
+			NBTCompoundList listAbility = abilities.getCompoundList("Abilities");
+			if(abilities != null) {
+				for(ReadWriteNBT ability : listAbility) {
+					String skill = ability.getString("skill");
+					String event = ability.getString("event");
+					int timer = ability.getInteger("timer");
+					player.sendMessage("Skill: " + skill + ", event: " + event + ", timer: " + timer);
+				}
+			}
 		}
 	}
 }
