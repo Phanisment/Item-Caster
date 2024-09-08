9a0996faefbb48d894c8d92750e955c6dbb6215f
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-08 20:23:26 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -7,6 +7,10 @@ import io.phanisment.itemcaster.SkillExecutor;
 public class ItemCaster extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		if(Bukkit.getPluginManager().getPlugin("MythicMobs") == null) {
+			getServer().getPluginManager().disablePlugin(this);
+			getLogger().info("Plugin Disabled, Need plugin MythicMobs ro enable this plugin.")
+		}
 		getLogger().info("Plugin Installed!");
 		
 		getServer().getPluginManager().registerEvents(new SkillExecutor(), this);
