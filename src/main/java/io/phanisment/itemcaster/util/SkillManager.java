c81151fdcfdae628efe26797acc0162c3e15882a
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-09 19:06:15 +0700

    update src/main/java/io/phanisment/itemcaster/util/SkillManager.java

--- src/main/java/io/phanisment/itemcaster/util/SkillManager.java
+++ src/main/java/io/phanisment/itemcaster/util/SkillManager.java
@@ -1,1 +1,41 @@
+package io.phanisment.itemcaster.util;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
+import de.tr7zw.changeme.nbtapi.NBTCompoundList;
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+
+import io.phanisment.itemcaster.MythicMobs;
+
+public class SkillManager {
+	public void runSkill(Player player, String event) {
+		ItemStack item = player.getInventory().getItemInMainHand();
+		NBTItem nbtItem = new NBTItem(item);
+		NBTCompoundList abilities = nbtItem.getCompoundList("Abilities");
+		if(abilities != null) {
+			for(ReadWriteNBT ability : abilities) {
+				String skill = ability.getString("skill");
+				String type = ability.getString("type");
+				int timer = ability.getInteger("timer");
+				
+				if (id != null && type != null && timer == null) {
+					this.activeSkill(player, skill, type, event);
+				//} else if (id != null && type == null && timer != null) {
+					//this.passiveSkill(skill, timer, cooldown);
+				}
+			}
+		}
+	}
+	
+	public void activeSkill(Player player, String skill, String type, String event) {
+			Boolean isSame = (type == event) ? true : false;
+			
+			if(isSame){
+				MythicMobs.runSkill(skill, player);
+			}
+		}
+	}
+}
