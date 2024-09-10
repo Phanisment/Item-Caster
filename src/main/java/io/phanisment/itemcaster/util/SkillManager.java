df71b4beb308b693c46cd347cd4653e0caffd4fb
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-10 20:05:06 +0700

    update src/main/java/io/phanisment/itemcaster/util/SkillManager.java

--- src/main/java/io/phanisment/itemcaster/util/SkillManager.java
+++ src/main/java/io/phanisment/itemcaster/util/SkillManager.java
@@ -10,37 +10,53 @@ import de.tr7zw.changeme.nbtapi.NBTItem;
 
 import io.phanisment.itemcaster.MythicMobs;
 
-import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SkillManager {
 	private Player player;
 	private String event;
+	
+	public String SKILL;
+	public String ACTION;
+	public Integer TIMER;
+	
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
 	public SkillManager(Player player, String event) {
 		this.player = player;
 		this.event = event;
 	}
 
-	public void runSkill() {
+	public SkillManager runSkill() {
 		ItemStack item = player.getInventory().getItemInMainHand();
 		NBTItem nbtItem = new NBTItem(item);
 		NBTCompoundList abilities = nbtItem.getCompoundList("Abilities");
-		
 		if (abilities != null) {
 			for (ReadWriteNBT ability : abilities) {
-				String skill = ability.getString("skill");
-				String type = ability.getString("type");
-				
-				if (skill != null && type != null) {
-					this.activeSkill(player, skill, type, event);
-				}
+				this.SKILL = ability.getString("skill").toLowerCase();
+				this.ACTION = ability.getString("action").toLowerCase();
+				this.TIMER = ability.getString("timer");
 			}
 		}
+		return this;
 	}
 
-	public void activeSkill(Player player, String skill, String type, String event) {
-		if (type.equals(event)) {
-			MythicMobs.runSkill(skill, player);
+	public void activeSkill() {
+		if (this.event == this.ACTION && this.SKILL != null || this.ACTION != null || this.TIMER == null) {
+			// Plans i want make is make a custom cooldown.
+			MythicMobs.runSkill();
+		}
+	}
+	
+	public void passiveSkill(Player player) {
+		if (this.event == this.ACTION && this.SKILL != null || this.ACTION != null || this.TIMER != null) {
+			long timer = this.TIMER;
+			long delay = timer * 50;
+			scheduler.schedule(() -> {
+				MythicMobs.runSkill();
+			}, delay, TimeUnit.MILLISECONDS);
 		}
 	}
 }
