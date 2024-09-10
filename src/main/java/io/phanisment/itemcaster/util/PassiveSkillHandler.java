e7be3b14e455122e02dd1cea7a23d2c068082af1
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-10 21:47:26 +0700

    update src/main/java/io/phanisment/itemcaster/util/PassiveSkillHandler.java

---
+++ src/main/java/io/phanisment/itemcaster/util/PassiveSkillHandler.java
@@ -0,0 +1,25 @@
+package io.phanisment.itemcaster.util;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.entity.Player;
+
+import io.phanisment.itemcaster.MythicMobs;
+
+public class SkillRunnable extends BukkitRunnable {
+	private final Player player;
+	private final String SKILL;
+	
+	public SkillRunnable(Player player, String skill) {
+		this.playe = player;
+		this.SKILL = skill;
+	}
+	
+	@Override
+	public void run() {
+		ItemStack item = this.player.getInventory().getItemInMainHand();
+		if (item != null) {
+			MythicMobs.runSkill(this.SKILL, this.player);
+		}
+	}
+}
