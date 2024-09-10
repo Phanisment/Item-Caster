e7776dfe88fad2538ccf7e24572971c50bf0bcf8
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-10 23:20:41 +0700

    update src/main/java/io/phanisment/itemcaster/util/PassiveSkill.java

---
+++ src/main/java/io/phanisment/itemcaster/util/PassiveSkill.java
@@ -0,0 +1,11 @@
+package io.phanisment.itemcaster.util;
+
+import io.phanisment.itemcaster.ItemCaster;
+
+public class PassiveSkill {
+	private final ItemCaster plugin;
+	
+	public PassiveSkill(ItemCaster plugin) {
+		this.plugin = plugin;
+	}
+}
