bb6f536865b549f018f95b38d7b05918d0a74a8f
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-10 07:30:15 +0700

    update src/main/java/io/phanisment/itemcaster/SkillExecutor.java

--- src/main/java/io/phanisment/itemcaster/SkillExecutor.java
+++ src/main/java/io/phanisment/itemcaster/SkillExecutor.java
@@ -13,8 +13,8 @@ public class SkillExecutor implements Listener {
 	public void onPlayerRightClick(PlayerInteractEvent event) {
 		if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getItem() != null) {
 			Player player = event.getPlayer();
-			SkillManager skill = new SkillManager();
-			skill.runSkill(player, "right_click");
+			SkillManager skill = new SkillManager(player, "right_click");
+			skill.runSkill();
 		}
 	}
 }
