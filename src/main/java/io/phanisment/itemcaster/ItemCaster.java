63bd5f8cdf9e0fd3240be554d4d15773d1d214c5
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-10 23:22:58 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -9,9 +9,12 @@ import io.phanisment.itemcaster.SkillExecutor;
 import io.phanisment.itemcaster.util.SkillManager;
 
 public class ItemCaster extends JavaPlugin {
+	private int sched;
+	
 	@Override
 	public void onEnable() {
 		getLogger().info("Plugin Installed!");
 		getServer().getPluginManager().registerEvents(new SkillExecutor(), this);
-	}
+		
+		getServer().getScheduler().runTaskTimer(this, new PassiveSkill(), 0L, 1L);
 }
