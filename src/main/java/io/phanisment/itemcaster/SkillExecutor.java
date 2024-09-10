eb64a0d3283dbbb6e3af5536c05c7e7a94752abf
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-10 09:22:54 +0700

    update src/main/java/io/phanisment/itemcaster/SkillExecutor.java

--- src/main/java/io/phanisment/itemcaster/SkillExecutor.java
+++ src/main/java/io/phanisment/itemcaster/SkillExecutor.java
@@ -2,7 +2,11 @@ package io.phanisment.itemcaster;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 
@@ -15,10 +19,44 @@ public class SkillExecutor implements Listener {
 			Player player = event.getPlayer();
 			SkillManager skill = new SkillManager(player, "right_click");
 			skill.runSkill();
-		} else if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getItem() != null) {
+		} else if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK && event.getItem() != null) {
 			Player player = event.getPlayer();
-			SkillManager skill = new SkillManager(player, "right_click");
+			SkillManager skill = new SkillManager(player, "left_click");
 			skill.runSkill();
 		}
+	}
+	
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent event) {
+		if (event.getEntity() instanceof Player) {
+			Player player = (Player) event.getEntity();
+			SkillManager skill = new SkillManager(player, "damaged");
+			skill.runSkill();
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerAttack(EntityDamageByEntityEvent event) {
+		if (event.getDamager() instanceof Player) {
+			Player player = (Player) event.getDamager();
+			SkillManager skill = new SkillManager(player, "attack");
+			skill.runSkill();
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerJump(PlayerMoveEvent event) {
+		Player player = event.getPlayer();
+		if (player.isOnGround() && event.getTo().getY() > event.getFrom().getY()) {
+			SkillManager skill = new SkillManager(player, "jump");
+			skill.runSkill();
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerSneak(PlayerToggleSneakEvent event) {
+		Player player = event.getPlayer();
+		SkillManager skill = new SkillManager(player, "sneak");
+		skill.runSkill();
 	}
 }
