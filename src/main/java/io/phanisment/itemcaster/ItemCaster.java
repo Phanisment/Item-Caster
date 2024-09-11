cee0425ab4addb1365fb6794bbef7809cf191a01
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-11 07:24:20 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -5,10 +5,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.inventory.ItemStack;
+
+import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
+import de.tr7zw.changeme.nbtapi.NBTCompoundList;
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+
 import io.phanisment.itemcaster.SkillExecutor;
 import io.phanisment.itemcaster.util.SkillManager;
 
 public class ItemCaster extends JavaPlugin {
+	private Integer Timer;
+	
 	private int sched;
 	
 	@Override
@@ -16,5 +25,7 @@ public class ItemCaster extends JavaPlugin {
 		getLogger().info("Plugin Installed!");
 		getServer().getPluginManager().registerEvents(new SkillExecutor(), this);
 		
-		getServer().getScheduler().runTaskTimer(this, new PassiveSkill(), 0L, 1L);
+		getServer().getScheduler().runTaskTimer(this, new PassiveSkill() {
+			
+		}, 0L, 1L);
 }
