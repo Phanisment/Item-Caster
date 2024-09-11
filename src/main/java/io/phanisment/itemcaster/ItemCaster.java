3a55d8b59239b446cd9e16d9a89aa5b4f6abc1ce
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-11 07:27:11 +0700

    update src/main/java/io/phanisment/itemcaster/ItemCaster.java

--- src/main/java/io/phanisment/itemcaster/ItemCaster.java
+++ src/main/java/io/phanisment/itemcaster/ItemCaster.java
@@ -16,16 +16,16 @@ import io.phanisment.itemcaster.SkillExecutor;
 import io.phanisment.itemcaster.util.SkillManager;
 
 public class ItemCaster extends JavaPlugin {
-	private Integer Timer;
-	
-	private int sched;
-	
+	private Long Timer;
+
 	@Override
 	public void onEnable() {
 		getLogger().info("Plugin Installed!");
 		getServer().getPluginManager().registerEvents(new SkillExecutor(), this);
 		
 		getServer().getScheduler().runTaskTimer(this, new PassiveSkill() {
-			
-		}, 0L, 1L);
+			for (Player player : Bukkit.getOnlinePlayers()) {
+				
+			}
+		}, 0L, this.Timer);
 }
