244c311e833c5da25cb9e6fc8bcfdbfb0ddbcdf9
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-12 07:29:45 +0700

    update src/main/java/io/phanisment/itemcaster/util/SkillManager.java

--- src/main/java/io/phanisment/itemcaster/util/SkillManager.java
+++ src/main/java/io/phanisment/itemcaster/util/SkillManager.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SkillManager {
-	private final Map<Player, Map<String, Integer>> skillTimers = new HashMap<>();
+	private Map<Player, Map<String, Integer>> skillTimers = new HashMap<>();
 	
 	public void runSkill(Player player, String event) {
 		ItemStack item = player.getInventory().getItemInMainHand();
