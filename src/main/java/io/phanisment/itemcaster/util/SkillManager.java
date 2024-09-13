57cc9657b388f52e2964112c45e5c275cf2826f0
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-13 15:09:25 +0700

    update src/main/java/io/phanisment/itemcaster/util/SkillManager.java

--- src/main/java/io/phanisment/itemcaster/util/SkillManager.java
+++ src/main/java/io/phanisment/itemcaster/util/SkillManager.java
@@ -17,10 +17,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SkillManager {
-	private final String skill;
-	private final String action;
-	private final int timer;
-	
 	private static Map<Player, Map<String, Integer>> skillTimers = new HashMap<>();
 
 	public void runSkill(Player player, String event) {
@@ -33,27 +29,29 @@ public class SkillManager {
 		NBTCompoundList abilities = nbtItem.getCompoundList("Abilities");
 		if (abilities != null) {
 			for (ReadWriteNBT ability : abilities) {
-				this.skill = ability.getString("skill");
-				this.action = ability.getString("action");
-				this.timer = ability.getInteger("timer");
-			}
-			
-			try {
-				if (event.equals(action) && skill != null && timer == 0 && action != null || action.trim().isEmpty()) {
-					MythicMobs.runSkill(skill, player);
-				} else if (event.equals("timer") && skill != null && timer != 0 && action == null || action.trim().isEmpty()) {
-					skillTimers.putIfAbsent(player, new HashMap<>());
-					int cooldown = skillTimers.get(player).getOrDefault(skill, 0);
-					
-					cooldown++;
-					if (cooldown >= timer) {
-						MythicMobs.runSkill(skill, player);
-						skillTimers.remove(player);
-					}
-					skillTimers.get(player).put(skill, cooldown);
-				}
-			} catch (Exception e) {
+				String skill = ability.getString("skill");
+				String action = ability.getString("action");
+				int timer = ability.getInteger("timer");
 				
+				try {
+					if (event.equals(action) && skill != null && timer == 0 && action != null || action.trim().isEmpty()) {
+						MythicMobs.runSkill(skill, player);
+					} else if (event.equals("timer") && skill != null && timer != 0 && action == null || action.trim().isEmpty()) {
+						optionalTimer.ifPresent(data -> {
+							skillTimers.putIfAbsent(player, new HashMap<>());
+							int cooldown = skillTimers.get(player).getOrDefault(skill, 0);
+							
+							cooldown++;
+							if (cooldown >= timer) {
+								MythicMobs.runSkill(skill, player);
+								skillTimers.remove(player);
+							}
+							skillTimers.get(player).put(skill, cooldown);
+						});
+					}
+				} catch (Exception e) {
+					
+				}
 			}
 		}
 	}
